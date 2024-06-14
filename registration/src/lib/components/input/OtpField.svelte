cf72dc25dc0c2cf2610a9cea5236376e86ee5187
Khaled Ferjani <kferjani@linagora.com>
2024-06-14 10:49:09 +0100

    🐛 Fixed otp field issues with mobile keyboard (#103) (#109) (#111)
    * 🐛 fix: otp input mobile issues
    
    fixed focus switch, paste, virtual keyboard issues
    
    * ➖ chore(deps): remove unused svelte-otp dependency

--- registration/src/lib/components/input/OtpField.svelte
+++ registration/src/lib/components/input/OtpField.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-	import SvelteOtp from '@k4ung/svelte-otp';
+	import OtpInput from '$components/otp/OtpInput.svelte';
 	export let value: string;
 </script>
 
-<SvelteOtp
-	numberOnly
+<OtpInput
 	inputClass="ring-2 ring-[#C9C5CA] focus:outline-none focus:ring-primary flex p-[10px] justify-center items-center rounded-[4px] w-[32.5px] text-center text-[17px] font-medium tracking-[0px] focus:ring-primary appearance-none"
 	wrapperClass="flex items-center justify-center gap-[8px] self-stretch [&>*:nth-child(even)]:mr-4 font-[Inter]"
 	numOfInputs={6}
