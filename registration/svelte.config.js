70a91277ec95d0d48bad61382eff43d8183f8986
Khaled FERJANI <kferjani@linagora.com>
2024-08-20 14:00:19 +0100

    🧑‍💻 chore: clean svelte config

--- registration/svelte.config.js
+++ registration/svelte.config.js
@@ -4,14 +4,9 @@ import preprocessReact from "svelte-preprocess-react/preprocessReact";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
   preprocess: [vitePreprocess(), preprocessReact()],
 
 	kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
     csrf: {
       checkOrigin: false
