01904af1c9986dc5be4d6c62742b7f5a9cd8c99d
Khaled FERJANI <kferjani@linagora.com>
2024-10-11 14:27:58 +0200

    🐛 fix: show go back button for login

--- registration/src/lib/components/display/FormHeader.svelte
+++ registration/src/lib/components/display/FormHeader.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
 	import NavigateBack from '$components/buttons/NavigateBack.svelte';
 	import AppLogo from '$components/logo/AppLogo.svelte';
-	import { activeTab, registrationStep, rewindRegistrationStep } from '$store';
+	import { activeTab, registrationStep, rewindStep } from '$store';
 
 	export let show: boolean = true;
 
 	$: showNavigation =
-		$registrationStep !== 'home' && $registrationStep !== 'confirmed' && $activeTab !== 'login';
+		($registrationStep !== 'home' && $registrationStep !== 'confirmed') || $activeTab === 'login';
 </script>
 
 <div class="flex flex-row">
-	<NavigateBack handler={rewindRegistrationStep} show={showNavigation} />
+	<NavigateBack handler={rewindStep} show={showNavigation} />
 	{#if show}
 		<div class="flex p-0 flex-col justify-center items-center self-stretch w-full">
 			<div class="flex w-full p-[12px] pt-[30px] flex-col items-center gap-[10px]">
