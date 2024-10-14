cf926ecc1ce2471a63356333256a636210c4b338
Khaled FERJANI <kferjani@linagora.com>
2024-10-14 17:56:59 +0200

    🎨 fix: change success info layout

--- registration/src/lib/components/user/SuccessInfo.svelte
+++ registration/src/lib/components/user/SuccessInfo.svelte
@@ -8,23 +8,12 @@
 <div
 	class="flex flex-col space-y-4 w-full items-center justify-center text-sm font-medium leading-5 tracking-wide text-center"
 >
-	<div
-		class="flex flex-col text-center text-[28px] not-italic font-semibold leading-9 text-[#1C1B1F]"
-	>
-		<h1 class="px-12">
-			{$t('Congratulations')},
-		</h1>
-		<h1 class="px-12">
-			{`${firstName} ${lastName}`}!
-		</h1>
+
+	<div class="w-full text-center text-disabled-text text-[22px] font-semibold leading-7">
+		Congratulations, <br />{`${firstName} ${lastName}`}!
 	</div>
 
-	<span
-		class="text-center text-[17px] not-italic font-medium leading-6 tracking-[-0.15px] text-[#818C99]"
-		>{$t('You are all set up')}</span
-	>
-	<span
-		class="text-center text-[17px] not-italic font-medium leading-6 tracking-[-0.15px] text-[#313033]"
-		>{$t('logged_twakeid_choose')}</span
-	>
+	<div class="w-full text-center text-[#98a0a8] text-base font-normal leading-normal">
+		{$t('logged_twakeid_choose')}
+	</div>
 </div>
