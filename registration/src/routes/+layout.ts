b4eaa12ec0d3b0bdd2cf60a01df600e0ab96c0cb
Khaled FERJANI <kferjani@linagora.com>
2024-10-14 10:43:43 +0200

    🐛 fix: saved language not being displayed in success route

--- registration/src/routes/+layout.ts
+++ registration/src/routes/+layout.ts
@@ -3,13 +3,17 @@ import { getLocaleFromNavigator, locale, waitLocale } from 'svelte-i18n';
 import type { LayoutLoad } from './$types';
 import '$lib/i18n';
 import logger from '$services/logger';
+import { get } from 'svelte/store';
+import { preferences } from '$store/preferences';
 
 export const load: LayoutLoad = async () => {
 	if (browser) {
 		const detectedLocale = getLocaleFromNavigator();
 		logger.info(`Detected locale from browser: ${detectedLocale}`);
 
-		locale.set(detectedLocale);
+		const userLanguage = get(preferences).locale;
+
+		locale.set(userLanguage ?? detectedLocale);
 	}
 
 	await waitLocale();
