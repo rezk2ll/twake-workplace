d7f2a0adb1c08830f7a8e3699ccacbb1b9a877ba
Khaled FERJANI <kferjani@linagora.com>
2024-10-15 09:56:05 +0200

    🐛 fix: hide form header for success page

--- registration/src/lib/components/landing/AccessSelect.svelte
+++ registration/src/lib/components/landing/AccessSelect.svelte
@@ -13,7 +13,8 @@
 	import LanguageSelector from '$components/display/i18n/LanguageSelector.svelte';
 	import { isMobile } from '$src/lib/utils/device';
 
-	$: showHeader = $registrationStep !== 'home' || $activeTab === 'login';
+	$: showHeader =
+		($registrationStep !== 'home' && $registrationStep !== 'success') || $activeTab === 'login';
 	$: appName = getAppName();
 	$: appUrl = getApplicationStoreUrl($app)?.url || '';
 	$: showDownloadBanner = $showBanner && $app !== 'default' && appUrl.length > 0;
