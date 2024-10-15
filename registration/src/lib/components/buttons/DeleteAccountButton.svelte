285cdb0462082f07c3c2b8a3f4a768ec5c7304d9
Khaled FERJANI <kferjani@linagora.com>
2024-10-15 15:05:14 +0200

    🎨 fix: update delete account button component

--- registration/src/lib/components/buttons/DeleteAccountButton.svelte
+++ registration/src/lib/components/buttons/DeleteAccountButton.svelte
@@ -3,17 +3,21 @@
 	import { sendAccountDeletionMail } from '$utils/account';
 	import DeleteAccountIcon from '$components/icons/DeleteAccountIcon.svelte';
 
-  export let username: string;
+	export let username: string;
 </script>
 
 <button
 	type="button"
 	on:click={() => sendAccountDeletionMail(username)}
-	class="w-full flex items-center text-disabled-text gap-5 justify-start p-2.5 text-sm transition-colors duration-200 transform hover:bg-red-300"
+	class="w-full flex items-center text-disabled-text gap-5 justify-start px-0 lg:px-2 lg:py-2 text-sm transition-colors duration-200 transform hover:bg-red-300"
 >
-	<DeleteAccountIcon />
+	<div class="w-full md:w-auto">
+		<DeleteAccountIcon />
+	</div>
 
 	<div class="mx-1 grow text-left">
-		<h1 class="text-sm font-semibold">{$t('delete-account')}</h1>
+		<div class="w-[296px] h-5 text-[#76787a] text-sm font-normal leading-tight tracking-tight">
+			{$t('delete-account')}
+		</div>
 	</div>
 </button>
