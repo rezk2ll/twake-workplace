586ab5a14527e6268a33c68dc235b82320cbf6d9
Khaled FERJANI <kferjani@linagora.com>
2024-10-15 15:05:45 +0200

    🚨 chore: lint

--- registration/src/lib/components/landing/LoggedHero.svelte
+++ registration/src/lib/components/landing/LoggedHero.svelte
@@ -2,13 +2,16 @@
 	import Logo from '$components/logo/Logo.svelte';
 	import { t, locale } from 'svelte-i18n';
 
-  $: direction = $locale === 'ar' ? 'rtl' : 'auto';
+	$: direction = $locale === 'ar' ? 'rtl' : 'auto';
 </script>
 
 <div class="flex flex-col space-y-8 pt-20 w-full max-w-xl">
 	<Logo />
 	<div class="flex flex-col space-y-5 w-full">
-		<h1 dir={direction} class="text-4xl text-black font-semibold leading-[52px] tracking-normal text-left rtl:text-right">
+		<h1
+			dir={direction}
+			class="text-4xl text-black font-semibold leading-[52px] tracking-normal text-left rtl:text-right"
+		>
 			{$t('logged_hero')}
 		</h1>
 	</div>
