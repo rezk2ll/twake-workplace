0e182f8c76a94bc85eff6dae9ee4fde70a502176
Khaled FERJANI <kferjani@linagora.com>
2024-10-15 15:35:21 +0200

    🎨 fix: success page layout

--- registration/src/routes/success/+page.svelte
+++ registration/src/routes/success/+page.svelte
@@ -4,6 +4,7 @@
 	import SuccessCard from '$components/landing/SuccessCard.svelte';
 	import LoggedUser from '$components/user/LoggedUser.svelte';
 	import LoggedHero from '$components/landing/LoggedHero.svelte';
+	import MobileLogout from '$src/lib/components/user/MobileLogout.svelte';
 
 	export let data: PageData;
 
@@ -12,7 +13,10 @@
 </script>
 
 <div class="w-full h-full lg:h-fit bg-white md:bg-transparent">
-	<div class="flex flex-col h-full lg:space-y-5">
+	<div class="relative flex flex-col h-full lg:space-y-5">
+		<div class="hidden md:block md:absolute lg:hidden top-7 right-9">
+			<MobileLogout />
+		</div>
 		<div class="h-20 flex-row w-full lg:p-10 hidden lg:flex">
 			<div class="w-full" />
 			<div class="right-0 w-56">
@@ -20,10 +24,12 @@
 			</div>
 		</div>
 		<div
-			class="flex w-full lg:h-full h-auto md:px-[120px] lg:px-8 xl:px-[155px] 3xl:px-72 4xl:px-96 lg:py-4"
+			class="flex w-full md:h-full h-auto md:px-[120px] lg:px-8 xl:px-[155px] 3xl:px-72 4xl:px-96 md:pt-[131px] lg:py-4"
 		>
 			<div class="w-full h-full">
-				<div class="flex flex-col-reverse lg:flex-row-reverse w-full lg:pt-9">
+				<div
+					class="flex flex-col-reverse lg:flex-row-reverse w-full lg:pt-9 items-center justify-center lg:justify-normal lg:items-baseline"
+				>
 					<SuccessCard {firstName} {lastName} {phone} {username} {email} />
 					<div class="hidden lg:flex w-full items-start justify-start">
 						<LoggedHero />
